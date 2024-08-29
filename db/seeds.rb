a571ba9bd58d5637c81a3b6cec98110caf190b44
Alexander Dubovoy <39504104+ajdubovoy@users.noreply.github.com>
2024-08-29 14:45:25 +0200

    URI.open -> URI.parse (#1)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,22 @@ Pokemon.destroy_all
 
 puts 'Creating trainers...'
 ash = Trainer.create(name: "Ash Ketchum", age: 18)
-ash.photo.attach(io: URI.open('https://upload.wikimedia.org/wikipedia/en/e/e4/Ash_Ketchum_Journeys.png'), filename: 'ash_ketchum.png', content_type: 'image/png')
+ash.photo.attach(io: URI.parse('https://upload.wikimedia.org/wikipedia/en/e/e4/Ash_Ketchum_Journeys.png').open, filename: 'ash_ketchum.png', content_type: 'image/png')
 puts "Ash has stepped up to the plate!"
 misty = Trainer.create(name: "Misty", age: 20)
-misty.photo.attach(io: URI.open('https://upload.wikimedia.org/wikipedia/en/b/b1/MistyEP.png'), filename: 'misty.png', content_type: 'image/png')
+misty.photo.attach(io: URI.parse('https://upload.wikimedia.org/wikipedia/en/b/b1/MistyEP.png').open, filename: 'misty.png', content_type: 'image/png')
 puts "Misty is ready to go!"
 brock = Trainer.create(name: "Brock", age: 22)
-brock.photo.attach(io: URI.open('https://upload.wikimedia.org/wikipedia/en/7/71/DP-Brock.png'), filename: 'brock.png', content_type: 'image/png')
+brock.photo.attach(io: URI.parse('https://upload.wikimedia.org/wikipedia/en/7/71/DP-Brock.png').open, filename: 'brock.png', content_type: 'image/png')
 puts "Brock is on the scene!"
 
 puts 'Creating pokemons...'
-response = URI.open('https://pokeapi.co/api/v2/pokemon?limit=50').read
+response = URI.parse('https://pokeapi.co/api/v2/pokemon?limit=50').read
 results = JSON.parse(response)['results']
 results.each do |result|
-  info = JSON.parse(URI.open(result['url']).read)
+  info = JSON.parse(URI.parse(result['url']).read)
   pokemon =Pokemon.create(name: info['name'].capitalize, element_type: info['types'].first['type']['name'])
-  pokemon.photo.attach(io: URI.open(info['sprites']['front_default']), filename: "#{info['name']}.png", content_type: 'image/png')
+  pokemon.photo.attach(io: URI.parse(info['sprites']['front_default']).open, filename: "#{info['name']}.png", content_type: 'image/png')
   puts "Screeahhhhh! #{pokemon.name} created!"
 end
 
